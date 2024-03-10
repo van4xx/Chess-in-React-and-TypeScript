8fb1bd61837977a77ddb0f164c24e18ce82390b8
Van4xx <van4xx@ya.ru>
2024-03-10 17:32:06 +0300

    Creating OOP classes for chess

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./App.css";
 import CellComponent from './components/CellComponent';
 import BoardComponent from './components/BoardComponent';
+import { Board } from './components/models/Board';
 
 const App = () => {
+  const [board, setBoard] = useState(new Board())
+
+  useEffect (() => {
+    restart()
+  }, [])
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells()
+    setBoard(newBoard)
+  }
+
   return (
     <div className='app'>
-      <BoardComponent />
-      <CellComponent />
+      <BoardComponent  
+        board={board}
+        setBoard={setBoard}
+      />
     </div>
   );
 };
---
+++ src/components/models/Board.ts
@@ -0,0 +1,23 @@
+import { Cell } from "./Cell";
+import { Colors } from "./Colors";
+import { Figure } from "./figures/Figure";
+
+export class Board {
+    cells: Cell[][] = []
+
+
+
+    public initCells() {
+        for (let i = 0; i < 8; i++){
+            const row: Cell[] = []
+            for (let j = 0; j < 8; j++){
+                if ((i + j) % 2 !== 0){
+                    row.push(new Cell(this, j, i, Colors.BLACK, null))  // black
+                } else {
+                    row.push(new Cell(this, j, i, Colors.WHITE, null))  // white
+                }
+            }
+            this.cells.push(row);
+        }
+    }
+}
