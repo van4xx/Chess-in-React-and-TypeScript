f628c0cb858234f4ee812c2e41d9edd28776db20
Van4xx <van4xx@ya.ru>
2024-03-12 19:36:51 +0300

    Game logic. Illumination of accessible cells

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Board } from './models/Board';
 import CellComponent from './CellComponent';
+import { Cell } from './models/Cell';
 
 interface BoardProps {
     board: Board;
@@ -8,6 +9,31 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+    const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+
+    function click(cell: Cell){
+        if(selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)){
+            selectedCell.moveFigure(cell);
+            setSelectedCell(null)
+        }
+        else {
+            setSelectedCell(cell);
+        }
+    }
+
+    useEffect(() => {
+        highlightCells()
+    }, [selectedCell])
+
+    function highlightCells(){
+        board.highlightCells(selectedCell)
+        updateBoard()
+    }
+
+    function updateBoard() {
+        const newBoard = board.getCopyBoard()
+        setBoard(newBoard)
+    }
 
     return (
         <div className="board">
@@ -15,8 +41,10 @@ const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
                 <React.Fragment key={index}>
                     {row.map(cell =>
                             <CellComponent 
+                                click={click}   
                                 cell={cell}
                                 key={cell.id}
+                                selected = {cell.x === selectedCell?.x && cell.y === selectedCell?.y}
                             />
                      )}
                 </React.Fragment>  
