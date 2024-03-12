4b55b365e3594e1f621af17b06ac8e0298229719
Van4xx <van4xx@ya.ru>
2024-03-13 00:19:50 +0300

    Make a Movement of knight and pawn

--- src/components/models/figures/Pawn.ts
+++ src/components/models/figures/Pawn.ts
@@ -5,6 +5,10 @@ import { Colors } from "../Colors";
 import { Cell } from "../Cell";
 
 export class Pawn extends Figure {
+
+    isFirstStep: boolean = true;
+
+
         constructor(color: Colors, cell: Cell){
         super(color, cell);
         this.logo = color === Colors.BLACK ? blacklogo : whitelogo;
@@ -13,7 +17,29 @@ export class Pawn extends Figure {
     canMove(target: Cell): boolean{
         if(!super.canMove(target))
             return false;
-        return true;
+        const direction = this.cell.figure?.color === Colors.BLACK ? 1: -1
+        const firstStepDirectoin = this.cell.figure?.color === Colors.BLACK ? 2: -2
+
+        if ((target.y === this.cell.y + direction || this.isFirstStep
+            && (target.y === this.cell.y + firstStepDirectoin))
+            && target.x === this.cell.x
+            && this.cell.board.getCell(target.x, target.y).isEmpty()){
+                return true;
+            }
+
+            if(target.y === this.cell.y + direction 
+            && (target.x === this.cell.x + 1 || target.x === this.cell.x - 1)
+            && this.cell.isEnemy(target)){
+                return true;
+            }
+
+        return false;
     }
+
+    moveFigure(target: Cell): void {
+        super.moveFigure(target);
+        this.isFirstStep = false
+    }
+
 }
 
